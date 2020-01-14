5f80442f8fa5221dfdcd500c993f944617b540bc
rameshbabu <crb96java@gmail.com>
2020-01-14 11:33:19 +0530

    completed

--- springbatch-mysqldb-to-csv/src/main/java/com/springbatch/config/SpringBatchConfig.java
+++ springbatch-mysqldb-to-csv/src/main/java/com/springbatch/config/SpringBatchConfig.java
@@ -1,15 +1,21 @@
 package com.springbatch.config;
 
-
 import javax.sql.DataSource;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import com.springbatch.model.Person;
 import com.springbatch.processor.PersonItemProcessor;
@@ -27,7 +33,7 @@ public class SpringBatchConfig {
 	@Autowired
 	private DataSource dataSource;
 	
-	//To read the data from db or any file
+	//item reader to read the data from db
 	@Bean
 	public JdbcCursorItemReader<Person> reader(){
 		
@@ -46,5 +52,35 @@ public class SpringBatchConfig {
 	}
 
 	//item writer
+	@Bean
+	public FlatFileItemWriter<Person> writer(){
+		
+		FlatFileItemWriter<Person> writer = new FlatFileItemWriter<Person>();
+		writer.setResource(new ClassPathResource("persons.csv"));
+		
+		//to separate person object values with ","
+		DelimitedLineAggregator<Person> lineAggregator = new DelimitedLineAggregator<Person>();
+		lineAggregator.setDelimiter(",");
+		
+		//it can person bean object value to person.csv file
+		BeanWrapperFieldExtractor<Person> wrapperFieldExtractor = new BeanWrapperFieldExtractor<Person>();
+		wrapperFieldExtractor.setNames(new String[] {"personId", "firstName", "lastName", "email", "age"});
+		
+		lineAggregator.setFieldExtractor(wrapperFieldExtractor);
+		
+		writer.setLineAggregator(lineAggregator);
+		return writer;
+	}
 	
+	//Step
+	@Bean
+	public Step step1() {
+		return stepBuilderFactory.get("step1").<Person, Person>chunk(100).reader(reader()).processor(processor()).writer(writer()).build();
+	}
+	
+	//Job
+	@Bean
+	public Job exportPersonJob() {
+		return jobBuilderFactory.get("exportPersonJob").incrementer(new RunIdIncrementer()).flow(step1()).end().build();
+	}
 }
